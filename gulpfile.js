69cd3ac4f9ed297f65bbdfb90c9eb8f2db46dcfb
gendrybaratheon <gendrybaratheon@163.com>
2016-08-27 15:33:34 +0800

    gulp 合并编译前端资源

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.config.sourcemaps = false; // 不生成 *.css.map 文件
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    // 复制文件到 resources/assets
+    mix.copy('vendor/bower_components/bootstrap/dist/css/bootstrap.css',
+        'resources/assets/css/');
+    mix.copy('vendor/bower_components/bootstrap/dist/js/bootstrap.js',
+        'resources/assets/js/');
+
+    mix.copy('vendor/bower_components/jquery/dist/jquery.js',
+        'resources/assets/js/');
+
+    // 迁移 CSS 文件
+    mix.styles(['bootstrap.css', 'front.css'], 'public/assets/css/app.css');
+    mix.styles(['bootstrap.css', 'admin.css'], 'public/assets/css/admin.css');
+
+    // 迁移 JS 文件
+    mix.scripts(['jquery.js', 'bootstrap.js'], 'public/assets/js/app.js');
+
+    // 添加版本号 -- 相对于 public 路径
+    mix.version([
+        'assets/css/app.css',
+        'assets/css/admin.css',
+        'assets/js/app.js'
+    ]);
+
 });
